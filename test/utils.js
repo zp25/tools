0c10ecf50da1ae2a32e9a0976de1d9937458f178
zp25 <zebrap25@gmail.com>
2016-12-14 11:19:55 +0800

    Update: unit test

---
+++ test/utils.js
@@ -0,0 +1,39 @@
+const should = require('chai').should();
+const filesize = require('../utils/filesize');
+
+describe('filesize', () => {
+  it('文件小于1kb时应该返回false', () => {
+    const size = Math.pow(1024, 1) - 42;
+    const result = filesize(size);
+
+    result.should.be.false;
+  });
+
+  it('文件大于等于1024gb时应该返回gb', () => {
+    const sizes = [Math.pow(1024, 4), Math.pow(1024, 4) + 42];
+    const result = sizes.map(size => filesize(size).slice(-2).toLowerCase());
+
+    result.should.satisfy(units => units.every(unit => unit === 'gb'));
+  });
+
+  it('正确转换其他大小的文件', () => {
+    const sizes = [
+      Math.pow(1024, 1),
+      Math.pow(1024, 1) + 42,
+      Math.pow(1024, 2),
+      Math.pow(1024, 2) + 42,
+      Math.pow(1024, 3),
+      Math.pow(1024, 3) + 42,
+    ];
+    const compare = ['kb', 'kb', 'mb', 'mb', 'gb', 'gb'];
+    const cb = (val, index) => val[0] < 1024 && val[1] === compare[index];
+
+    const result = sizes.map((size) => {
+      const r = filesize(size);
+
+      return [Number(r.slice(0, -2)), r.slice(-2).toLowerCase()];
+    });
+
+    result.should.satisfy(vals => vals.every(cb));
+  });
+});
--- utils/filesize.js
+++ utils/filesize.js
@@ -1,11 +1,19 @@
-module.exports = (size) => {
-  const unit = ['b', 'kb', 'mb', 'gb'];
+/**
+ * 文件大小使用合适的单位表示
+ * @param {Number} size 文件大小，Bytes
+ * @return {(String|Boolean)} 转化结果，若小于1k返回false，最大单位gb
+ */
+const filesize = (size) => {
+  const units = ['b', 'kb', 'mb', 'gb'];
+  const limit = units.length - 1;
   let r = size;
 
   let i;
-  for (i = 0; r >= 1024; i++) {
+  for (i = 0; r >= 1024 && i < limit; i++) {
     r /= 1024;
   }
 
-  return i === 0 ? false : r.toFixed(2) + unit[i];
+  return i === 0 ? false : r.toFixed(2) + units[i];
 };
+
+module.exports = filesize;
